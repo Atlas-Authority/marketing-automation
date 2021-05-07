00c2651becc6f3c4de85aa14e2fd31ba70002644
Steven Degutis <sbdegutis@gmail.com>
2021-05-07 11:54:17 -0500

    Close deals that are ignored by new engine rules.

---
+++ bin/close-ignored-deals.js
@@ -0,0 +1,44 @@
+import CachedFileDownloader from '../lib/downloader/cached-file-downloader.js';
+import { downloadInitialData } from '../lib/downloader/download-initial-data.js';
+import LiveUploader from '../lib/uploader/live-uploader.js';
+import { DealStage } from '../lib/util/config.js';
+import * as datadir from '../lib/util/datadir.js';
+import * as logger from '../lib/util/logger.js';
+
+const data = await downloadInitialData({
+  downloader: new CachedFileDownloader()
+});
+
+const uploader = new LiveUploader();
+
+/** @type {(License & {reason: string})[][]} */
+const ignored = datadir.readJsonFile('out', 'ignored.json');
+
+const openDeals = data.allDeals.filter(deal => deal.properties.dealstage === DealStage.EVAL);
+
+const dealsToClose = (openDeals
+  .map(deal => ({
+    deal,
+    ignoredGroup: ignored.filter(group =>
+      group.some(row =>
+        row.addonLicenseId === deal.properties.addonlicenseid))[0],
+  }))
+  .filter(({ ignoredGroup }) => ignoredGroup)
+  .map(({ deal, ignoredGroup }) => ({
+    id: deal.id,
+    dealname: deal.properties.dealname,
+    stage: ignoredGroup.every(row => row.licenseType === 'EVALUATION')
+      ? DealStage.CLOSED_LOST
+      : DealStage.CLOSED_WON,
+    reasonsIgnored: ignoredGroup.map(row => row.reason),
+  }))
+);
+
+logger.info('Deals', dealsToClose);
+
+uploader.updateAllDeals(dealsToClose.map(row => ({
+  id: row.id,
+  properties: {
+    dealstage: row.stage,
+  },
+})));
