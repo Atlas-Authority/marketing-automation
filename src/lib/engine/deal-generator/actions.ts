fb791da9d6eef3dad9849d34b545d3ccdb54b87e
Steven Degutis <sbdegutis@gmail.com>
2021-09-13 16:28:33 -0500

    Turning decision matrix into a (partially implemented) decision matrix.

--- src/lib/engine/deal-generator/actions.ts
+++ src/lib/engine/deal-generator/actions.ts
@@ -1,3 +1,4 @@
+import { decisionMatrix } from "./decision-matrix.js";
 import { DealRelevantEvent } from "./events.js";
 
 export class ActionGenerator {
@@ -17,77 +18,9 @@ export class ActionGenerator {
   }
 
   generateFrom(events: DealRelevantEvent[], groups: RelatedLicenseSet) {
-    const licenseDeals = new Set<Deal>();
-    for (const license of groups.map(g => g.license)) {
-      const deal = this.allLicenseDeals.get(license.addonLicenseId);
-      if (deal) licenseDeals.add(deal);
-    }
-
-    const transactionDeals = new Set<Deal>();
-    for (const transaction of groups.flatMap(g => g.transactions)) {
-      const deal = this.allTransactionDeals.get(transaction.transactionId);
-      if (deal) transactionDeals.add(deal);
-    }
-
-    // Hosting     State         Event              Action
-    // ---------   -----------   ----------------   ----------------------------
-    // Server/DC   No Deal       Eval               Create Eval
-    // Server/DC   Eval Deal     Eval               Update Eval (CloseDate)
-    // Server/DC   No Deal       Purchase           Create Won (Amount)
-    // Server/DC   Eval Deal     Purchase           Close Won (Amount, CloseDate)
-    // Server/DC                 Renew/Up/Down      Create Won (Amount)
-    // Cloud       No Deal       Eval               Create Eval
-    // Cloud                     Purchase           Create or Update Closed Won (CloseDate)
-    // Cloud                     Renew/Up/Down      Create Won, (Amount)
-    // Any         Closed Deal   All Txs Refunded   Update (CloseDate, Amount, Stage=Lost)
-
-    const hosting = groups[0].license.hosting;
     for (const event of events) {
 
-      switch (event.type) {
-        case 'eval':
-
-          switch (hosting) {
-            case 'Server':
-            case 'Data Center':
-              // If existing eval deal:
-              //   Update CloseDate
-              // Else:
-              //   Create Eval
-              break;
-            case 'Cloud':
-              // If no existing eval deal:
-              //   Create Eval
-              break;
-          }
-
-          break;
-        case 'purchase':
-
-          switch (hosting) {
-            case 'Server':
-            case 'Data Center':
-              break;
-            case 'Cloud':
-              break;
-          }
-
-          break;
-        case 'renewal':
-        case 'upgrade':
-
-          switch (hosting) {
-            case 'Server':
-            case 'Data Center':
-              break;
-            case 'Cloud':
-              break;
-          }
-
-          break;
-        case 'refund':
-          break;
-      }
+      // processEvent(event, groups, decisionMatrix);
 
     }
 
---
+++ src/lib/engine/deal-generator/decision-matrix.ts
@@ -0,0 +1,65 @@
+import { DealRelevantEvent } from "./events.js";
+
+const hosting = {
+  isServer: (groups: RelatedLicenseSet) => getHosting(groups) === 'Server',
+  isDataCenter: (groups: RelatedLicenseSet) => getHosting(groups) === 'Data Center',
+  isCloud: (groups: RelatedLicenseSet) => getHosting(groups) === 'Cloud',
+  isAny: (_groups: RelatedLicenseSet) => true,
+};
+
+const event = {
+  isNewTrial: (event: DealRelevantEvent) => event.type === 'eval',
+  isPurchase: (event: DealRelevantEvent) => event.type === 'purchase',
+  isaRenewal: (event: DealRelevantEvent) => event.type === 'renewal',
+  isUpgraded: (event: DealRelevantEvent) => event.type === 'upgrade',
+  isRefunded: (event: DealRelevantEvent) => event.type === 'refund',
+};
+
+const state = {
+  hasNothing: (event: DealRelevantEvent, deals: Deal[]) => true,
+  hasTrial: (event: DealRelevantEvent, deals: Deal[]) => true,
+  any: (_event: DealRelevantEvent, _deals: Deal[]) => true,
+};
+
+const outcome = {
+  createTrial: (event: DealRelevantEvent, deal: Deal | null) => null,
+  updateCloseDate: (event: DealRelevantEvent, deal: Deal | null) => null,
+  createWon: (event: DealRelevantEvent, deal: Deal | null) => null,
+  closeWon: (event: DealRelevantEvent, deal: Deal | null) => null,
+  closeLost: (event: DealRelevantEvent, deal: Deal | null) => null,
+};
+
+type DecisionMatrix = [
+  (groups: RelatedLicenseSet) => boolean,
+  (event: DealRelevantEvent) => boolean,
+  (event: DealRelevantEvent, deals: Deal[]) => boolean,
+  (event: DealRelevantEvent, deal: Deal | null) => unknown,
+][];
+
+export const decisionMatrix: DecisionMatrix = [
+  [hosting.isServer, event.isNewTrial, state.hasNothing, outcome.createTrial],
+  [hosting.isServer, event.isNewTrial, state.hasTrial, outcome.updateCloseDate],
+  [hosting.isServer, event.isPurchase, state.hasNothing, outcome.createWon],
+  [hosting.isServer, event.isPurchase, state.hasTrial, outcome.closeWon],
+  [hosting.isServer, event.isaRenewal, state.any, outcome.createWon],
+  [hosting.isServer, event.isUpgraded, state.any, outcome.createWon],
+
+  [hosting.isDataCenter, event.isNewTrial, state.hasNothing, outcome.createTrial],
+  [hosting.isDataCenter, event.isNewTrial, state.hasTrial, outcome.updateCloseDate],
+  [hosting.isDataCenter, event.isPurchase, state.hasNothing, outcome.createWon],
+  [hosting.isDataCenter, event.isPurchase, state.hasTrial, outcome.closeWon],
+  [hosting.isDataCenter, event.isaRenewal, state.any, outcome.createWon],
+  [hosting.isDataCenter, event.isUpgraded, state.any, outcome.createWon],
+
+  [hosting.isCloud, event.isNewTrial, state.hasNothing, outcome.createTrial],
+  [hosting.isCloud, event.isPurchase, state.hasTrial, outcome.closeWon],
+  [hosting.isCloud, event.isPurchase, state.hasNothing, outcome.createWon],
+  [hosting.isCloud, event.isaRenewal, state.any, outcome.createWon],
+  [hosting.isCloud, event.isUpgraded, state.any, outcome.createWon],
+
+  [hosting.isAny, event.isRefunded, state.any, outcome.closeLost],
+];
+
+function getHosting(groups: RelatedLicenseSet) {
+  return groups[0].license.hosting;
+}
