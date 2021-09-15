dc73b1b367ff744c08f989c0a106cfc3424b15b0
Steven Degutis <sbdegutis@gmail.com>
2021-09-15 12:36:04 -0500

    Renaming decision matrix events for consistency.

--- src/lib/engine/deal-generator/decision-matrix.ts
+++ src/lib/engine/deal-generator/decision-matrix.ts
@@ -9,10 +9,10 @@ const hosting = {
 };
 
 const event = {
-  isNewTrial: (type: DealRelevantEvent['type']) => type === 'eval',
+  isTrial: (type: DealRelevantEvent['type']) => type === 'eval',
   isPurchase: (type: DealRelevantEvent['type']) => type === 'purchase',
-  isaRenewal: (type: DealRelevantEvent['type']) => type === 'renewal',
-  isUpgraded: (type: DealRelevantEvent['type']) => type === 'upgrade',
+  isRenewal: (type: DealRelevantEvent['type']) => type === 'renewal',
+  isUpgrade: (type: DealRelevantEvent['type']) => type === 'upgrade',
   isRefunded: (type: DealRelevantEvent['type']) => type === 'refund',
 };
 
@@ -42,25 +42,25 @@ const outcome = {
 };
 
 export const decisionMatrix: DecisionMatrix = [
-  [hosting.isServer, event.isNewTrial, state.hasNothing, outcome.createTrial],
-  [hosting.isServer, event.isNewTrial, state.hasTrial, outcome.update],
+  [hosting.isServer, event.isTrial, state.hasNothing, outcome.createTrial],
+  [hosting.isServer, event.isTrial, state.hasTrial, outcome.update],
   [hosting.isServer, event.isPurchase, state.hasNothing, outcome.createWon],
   [hosting.isServer, event.isPurchase, state.hasTrial, outcome.closeWon],
-  [hosting.isServer, event.isaRenewal, state.any, outcome.createWon],
-  [hosting.isServer, event.isUpgraded, state.any, outcome.createWon],
+  [hosting.isServer, event.isRenewal, state.any, outcome.createWon],
+  [hosting.isServer, event.isUpgrade, state.any, outcome.createWon],
 
-  [hosting.isDataCenter, event.isNewTrial, state.hasNothing, outcome.createTrial],
-  [hosting.isDataCenter, event.isNewTrial, state.hasTrial, outcome.update],
+  [hosting.isDataCenter, event.isTrial, state.hasNothing, outcome.createTrial],
+  [hosting.isDataCenter, event.isTrial, state.hasTrial, outcome.update],
   [hosting.isDataCenter, event.isPurchase, state.hasNothing, outcome.createWon],
   [hosting.isDataCenter, event.isPurchase, state.hasTrial, outcome.closeWon],
-  [hosting.isDataCenter, event.isaRenewal, state.any, outcome.createWon],
-  [hosting.isDataCenter, event.isUpgraded, state.any, outcome.createWon],
+  [hosting.isDataCenter, event.isRenewal, state.any, outcome.createWon],
+  [hosting.isDataCenter, event.isUpgrade, state.any, outcome.createWon],
 
-  [hosting.isCloud, event.isNewTrial, state.hasNothing, outcome.createTrial],
+  [hosting.isCloud, event.isTrial, state.hasNothing, outcome.createTrial],
   [hosting.isCloud, event.isPurchase, state.hasTrial, outcome.closeWon],
   [hosting.isCloud, event.isPurchase, state.hasNothing, outcome.createWon],
-  [hosting.isCloud, event.isaRenewal, state.any, outcome.createWon],
-  [hosting.isCloud, event.isUpgraded, state.any, outcome.createWon],
+  [hosting.isCloud, event.isRenewal, state.any, outcome.createWon],
+  [hosting.isCloud, event.isUpgrade, state.any, outcome.createWon],
 
   [hosting.isAny, event.isRefunded, state.hasNonLost, outcome.closeLost],
 ];
