25ccfcd9e2615269119c8c1f4ca7996da0eb9114
Steven Degutis <sbdegutis@gmail.com>
2021-10-15 11:34:00 -0500

    WIP: Starting to fill in entity manager functionality.

--- src/lib/model/hubspot/manager.ts
+++ src/lib/model/hubspot/manager.ts
@@ -1,3 +1,4 @@
+import * as hubspot from '@hubspot/api-client';
 import { HubspotEntity } from "./entity.js";
 
 export type HubspotInputObject = {
@@ -25,11 +26,29 @@ export abstract class HubspotEntityManager<
   I extends HubspotInputObject>
 {
 
+  constructor(private client: hubspot.Client) { }
+
   abstract kind: HubspotEntityKind;
   abstract associations: [keyof E, HubspotEntityKind][];
 
   abstract apiProperties: string[];
   abstract fromAPI(data: I['properties']): P;
   abstract toAPI(props: P): I['properties'];
+
+  api() {
+    switch (this.kind) {
+      case 'deal': return this.client.crm.deals;
+      case 'company': return this.client.crm.companies;
+      case 'contact': return this.client.crm.contacts;
+    }
+  }
+
+  async downloadAllEntities() {
+    let associations: undefined | string[];
+    if (this.associations.length > 0) {
+      associations = this.associations.map(a => a[1]);
+    }
+    const data = await this.api().getAll(undefined, undefined, this.apiProperties, associations);
+  }
 
 }
