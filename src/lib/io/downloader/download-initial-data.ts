4e8e058dfb61460b4fcd4deff4a9c4563734c667
Steven Degutis <sbdegutis@gmail.com>
2021-10-16 11:28:19 -0500

    Improvements to Atlassian data characteristics.

--- src/lib/io/downloader/download-initial-data.ts
+++ src/lib/io/downloader/download-initial-data.ts
@@ -238,7 +238,7 @@ const licensesWithoutDataInsightsSchema: Array<['every' | 'some', (license: RawL
   ['every', license => isNonBlankString(license?.tier)],
 
   ['every', license => !!license?.contactDetails],
-  ['some', license => isNonBlankString(license?.contactDetails?.company)],
+  ['every', license => isNonBlankString(license?.contactDetails?.company)],
   ['every', license => isNonBlankString(license?.contactDetails?.country)],
   ['every', license => isNonBlankString(license?.contactDetails?.region)],
 
@@ -258,7 +258,7 @@ const licensesWithoutDataInsightsSchema: Array<['every' | 'some', (license: RawL
 
   ['some', transaction => !!transaction?.partnerDetails],
   ['every', transaction => !transaction?.partnerDetails || isNonBlankString(transaction?.partnerDetails?.partnerName)],
-  ['some', transaction => !transaction?.partnerDetails || isNonBlankString(transaction?.partnerDetails?.partnerType)],
+  ['every', transaction => !transaction?.partnerDetails || isNonBlankString(transaction?.partnerDetails?.partnerType)],
 
   ['some', license => !!license?.partnerDetails?.billingContact],
   ['every', license => !license?.partnerDetails?.billingContact || isNonBlankString(license?.partnerDetails?.billingContact.email)],
