fe606738dcfd19a8ccdee8dc5d087a5b7ec60fad
Steven Degutis <sbdegutis@gmail.com>
2021-10-27 09:09:54 -0500

    Updating in-memory data via Uploader functions in MemoryRemote for more accurate double-run testing.

--- src/lib/io/memory-remote.ts
+++ src/lib/io/memory-remote.ts
@@ -1,7 +1,7 @@
 import * as datadir from '../cache/datadir.js';
 import log, { LogLevel, logLevel } from '../log/logger.js';
 import { RawLicense, RawTransaction } from "../model/marketplace/raw.js";
-import { Association, EntityKind, ExistingEntity, FullEntity, NewEntity } from "./hubspot.js";
+import { Association, EntityKind, ExistingEntity, FullEntity, NewEntity, RelativeAssociation } from "./hubspot.js";
 import { Downloader, Progress, Uploader } from "./interfaces.js";
 
 export class MemoryRemote implements Downloader, Uploader {
@@ -65,7 +65,12 @@ export class MemoryRemote implements Downloader, Uploader {
     const objects = inputs.map((o) => ({
       properties: o.properties,
       id: this.newUniqueId(kind),
+      associations: [],
     }));
+
+    const array = this.arrayFor(kind);
+    array.push(...objects);
+
     this.fakeApiConsoleLog(`Fake-created ${kind}s:`, objects);
     return objects;
   }
@@ -77,20 +82,48 @@ export class MemoryRemote implements Downloader, Uploader {
   }
 
   async updateHubspotEntities(kind: EntityKind, inputs: ExistingEntity[]): Promise<ExistingEntity[]> {
+    for (const input of inputs) {
+      const entity = this.getEntity(kind, input.id);
+      Object.assign(entity, input.properties);
+    }
+
     this.fakeApiConsoleLog(`Fake-updated ${kind}s:`, inputs);
     return inputs;
   }
 
   async createHubspotAssociations(fromKind: EntityKind, toKind: EntityKind, inputs: Association[]): Promise<void> {
+    for (const input of inputs) {
+      const entity = this.getEntity(fromKind, input.fromId);
+      const assoc: RelativeAssociation = `${toKind}:${input.toId}`;
+      if (!entity.associations.includes(assoc)) {
+        entity.associations.push(assoc);
+      }
+    }
+
     this.fakeApiConsoleLog(`Fake Associating ${fromKind}s to ${toKind}s:`, inputs);
   }
 
   async deleteHubspotAssociations(fromKind: EntityKind, toKind: EntityKind, inputs: Association[]): Promise<void> {
+    for (const input of inputs) {
+      const entity = this.getEntity(fromKind, input.fromId);
+      const assoc: RelativeAssociation = `${toKind}:${input.toId}`;
+      const idx = entity.associations.indexOf(assoc);
+      if (idx !== -1) {
+        entity.associations.splice(idx, 1);
+      }
+    }
+
     this.fakeApiConsoleLog(`Fake Unassociating ${fromKind}s to ${toKind}s:`, inputs);
   }
 
   private fakeApiConsoleLog(title: string, data: unknown[]) {
     log.info('Fake Uploader', title, this.verbose ? data : data.length);
+  }
+
+  private getEntity(kind: EntityKind, id: string) {
+    const entity = this.arrayFor(kind).find(e => e.id === id);
+    if (!entity) throw new Error(`Entity kind=${kind} id=${id} doesn't exist in test environment`);
+    return entity;
   }
 
   // Both
