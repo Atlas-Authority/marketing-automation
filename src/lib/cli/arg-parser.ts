46b056393eed7e2feb130cd0636532c6cecb145c
Steven Degutis <sbdegutis@gmail.com>
2021-10-27 08:25:18 -0500

    Getting strings from CLI parsing rather than objects which we can derive ourselves.

--- src/lib/cli/arg-parser.ts
+++ src/lib/cli/arg-parser.ts
@@ -15,15 +15,15 @@ class ArgParser {
     return value;
   }
 
-  getChoiceOrFail<T>(option: string, choices: { [opt: string]: () => T }) {
-    const value = this.get(option);
-    if (!value || !choices[value]) {
-      console.log(`Error: ${option} must be ${Object.keys(choices)
+  getChoiceOrFail<T extends string>(option: string, choices: T[]): T {
+    const value = this.get(option) as T;
+    if (!value || !choices.includes(value)) {
+      console.log(`Error: ${option} must be ${choices
         .map(c => `'${c}'`)
         .join(' or ')}`);
       process.exit(1);
     }
-    return choices[value]();
+    return value;
   }
 
   failIfExtraOpts() {
--- src/lib/cli/index.ts
+++ src/lib/cli/index.ts
@@ -8,14 +8,32 @@ import { LogLevel, logLevel } from "../log/logger.js";
 import { sharedArgParser } from './arg-parser.js';
 
 export function getIoFromCli() {
+  const downloaderArg = sharedArgParser.getChoiceOrFail('--downloader', [
+    'cached',
+    'live'
+  ]);
+
+  const uploaderArg = sharedArgParser.getChoiceOrFail('--uploader', [
+    'console',
+    'live'
+  ]);
+
   return {
-    downloader: sharedArgParser.getChoiceOrFail<Downloader>('--downloader', {
-      'live': () => new LiveDownloader(),
-      'cached': () => new CachedFileDownloader(),
-    }),
-    uploader: sharedArgParser.getChoiceOrFail<Uploader>('--uploader', {
-      'live': () => new LiveUploader(),
-      'console': () => new ConsoleUploader({ verbose: logLevel >= LogLevel.Verbose }),
-    }),
+    downloader: getDownloader(downloaderArg),
+    uploader: getUploader(uploaderArg),
   };
+}
+
+function getDownloader(kind: 'cached' | 'live'): Downloader {
+  switch (kind) {
+    case 'live': return new LiveDownloader();
+    case 'cached': return new CachedFileDownloader();
+  }
+}
+
+function getUploader(kind: 'console' | 'live'): Uploader {
+  switch (kind) {
+    case 'live': return new LiveUploader();
+    case 'console': return new ConsoleUploader({ verbose: logLevel >= LogLevel.Verbose });
+  }
 }
