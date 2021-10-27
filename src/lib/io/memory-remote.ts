729e35de6adacafc103dde71a2f3e4c829e1a1e2
Steven Degutis <sbdegutis@gmail.com>
2021-10-27 09:09:29 -0500

    Caching disk data into MemoryRemote in constructor so we can update it via Uploader functions.

--- src/lib/io/memory-remote.ts
+++ src/lib/io/memory-remote.ts
@@ -9,34 +9,54 @@ export class MemoryRemote implements Downloader, Uploader {
   verbose: boolean;
   ids = new Map<string, number>();
 
+  contacts: FullEntity[];
+  companies: FullEntity[];
+  deals: FullEntity[];
+  licensesWith: RawLicense[];
+  licensesWithout: RawLicense[];
+  transactions: RawTransaction[];
+  domains: string[];
+  tlds: string[];
+
   // Downloader
 
   constructor(opts?: { verbose?: boolean }) {
     this.verbose = opts?.verbose ?? logLevel >= LogLevel.Verbose;
+
+    this.deals = datadir.readJsonFile('in', `deal.json`);
+    this.companies = datadir.readJsonFile('in', `company.json`);
+    this.contacts = datadir.readJsonFile('in', `contact.json`);
+
+    this.licensesWith = datadir.readJsonFile('in', 'licenses-with.json');
+    this.licensesWithout = datadir.readJsonFile('in', 'licenses-without.json');
+    this.transactions = datadir.readJsonFile('in', 'transactions.json');
+
+    this.tlds = datadir.readJsonFile('in', 'tlds.json');
+    this.domains = datadir.readJsonFile('in', 'domains.json');
   }
 
   async downloadHubspotEntities(_progress: Progress, kind: EntityKind, apiProperties: string[], inputAssociations: string[]): Promise<FullEntity[]> {
-    return datadir.readJsonFile('in', `${kind}.json`);
+    return this.arrayFor(kind);
   }
 
   async downloadFreeEmailProviders(): Promise<string[]> {
-    return datadir.readJsonFile('in', 'domains.json');
+    return this.domains;
   }
 
   async downloadAllTlds(): Promise<string[]> {
-    return datadir.readJsonFile('in', 'tlds.json');
+    return this.tlds;
   }
 
   async downloadTransactions(): Promise<RawTransaction[]> {
-    return datadir.readJsonFile('in', 'transactions.json');
+    return this.transactions;
   }
 
   async downloadLicensesWithoutDataInsights(): Promise<RawLicense[]> {
-    return datadir.readJsonFile('in', 'licenses-without.json');
+    return this.licensesWithout;
   }
 
   async downloadLicensesWithDataInsights(): Promise<RawLicense[]> {
-    return datadir.readJsonFile('in', 'licenses-with.json');
+    return this.licensesWith;
   }
 
   // Uploader
@@ -69,8 +89,18 @@ export class MemoryRemote implements Downloader, Uploader {
     this.fakeApiConsoleLog(`Fake Unassociating ${fromKind}s to ${toKind}s:`, inputs);
   }
 
-  fakeApiConsoleLog(title: string, data: unknown[]) {
+  private fakeApiConsoleLog(title: string, data: unknown[]) {
     log.info('Fake Uploader', title, this.verbose ? data : data.length);
+  }
+
+  // Both
+
+  private arrayFor(kind: EntityKind) {
+    switch (kind) {
+      case 'company': return this.companies;
+      case 'contact': return this.contacts;
+      case 'deal': return this.deals;
+    }
   }
 
 }
