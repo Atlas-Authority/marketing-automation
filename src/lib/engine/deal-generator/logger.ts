2a0273074159a3cacf18ea8f8eb446901db34755
Steven Degutis <sbdegutis@gmail.com>
2021-11-29 14:21:24 -0600

    Centralizing redaction logic and making it more flexible.

--- src/lib/engine/deal-generator/logger.ts
+++ src/lib/engine/deal-generator/logger.ts
@@ -8,26 +8,22 @@ import { formatMoney } from "../../util/formatters.js";
 import { Action } from "./actions.js";
 import { abbrEventDetails, DealRelevantEvent } from "./events.js";
 
-type RedactIdFn = <T extends string | undefined | null>(prefix: string, id: T) => T;
-
-const sameId = <T extends string | undefined | null>(prefix: string, x: T) => x;
-
 const dealPropertyRedactors: {
-  [K in keyof DealData]: (redact: RedactIdFn, val: Partial<DealData>[K]) => Partial<DealData>[K]
+  [K in keyof DealData]: (redact: Redactor, val: Partial<DealData>[K]) => Partial<DealData>[K]
 } = {
-  addonLicenseId: (redact, addonLicenseId) => redact('L_', addonLicenseId),
+  addonLicenseId: (redact, addonLicenseId) => redact.addonLicenseId(addonLicenseId),
   amount: (redact, amount) => amount,
-  app: (redact, app) => redact('App:', app),
+  app: (redact, app) => redact.appName(app),
   closeDate: (redact, closeDate) => closeDate,
   country: (redact, country) => country,
-  dealName: (redact, dealName) => redact('DealName:', dealName),
+  dealName: (redact, dealName) => redact.dealName(dealName),
   dealStage: (redact, dealStage) => dealStage,
   deployment: (redact, deployment) => deployment,
   licenseTier: (redact, licenseTier) => licenseTier,
   origin: (redact, origin) => origin,
   pipeline: (redact, pipeline) => pipeline,
-  relatedProducts: (redact, relatedProducts) => redact('RelatedProducts:', relatedProducts),
-  transactionId: (redact, transactionId) => redact('TX_', transactionId),
+  relatedProducts: (redact, relatedProducts) => redact.product(relatedProducts),
+  transactionId: (redact, transactionId) => redact.transactionId(transactionId),
 };
 
 export class DealDataLogger {
@@ -35,43 +31,29 @@ export class DealDataLogger {
   plainLog = DataDir.out.file('deal-generator.txt').writeStream();
   rededLog = DataDir.out.file('deal-generator-redacted.txt').writeStream();
 
-  redactions = new Map<string, string>();
-  newIds = new Set<string>();
+  nonRedactor = new NonRedactor();
+  shhRedactor = new PrivacyRedactor();
 
   close() {
     this.plainLog.close();
     this.rededLog.close();
   }
 
-  redact<T extends string | undefined | null>(prefix: string, id: T): T {
-    if (id === undefined || id === null) return id;
-
-    let rid = this.redactions.get(id);
-    if (!rid) {
-      do { rid = prefix + uuidv4().replace(/-/g, '').slice(0, 10); }
-      while (this.newIds.has(rid));
-      this.newIds.add(rid);
-      this.redactions.set(id, rid)
-    };
-    return rid as T;
-  }
-
   logActions(actions: Action[]) {
-    const redact = this.redact.bind(this);
-    this._logActions(this.plainLog, sameId, actions);
-    this._logActions(this.rededLog, redact, actions);
+    this._logActions(this.plainLog, this.nonRedactor, actions);
+    this._logActions(this.rededLog, this.shhRedactor, actions);
   }
 
-  printDealProperties(log: LogWriteStream, redact: RedactIdFn, data: Partial<DealData>) {
+  printDealProperties(log: LogWriteStream, redact: Redactor, data: Partial<DealData>) {
     for (const [k, v] of Object.entries(data)) {
       const key = k as keyof DealData;
-      const fn = dealPropertyRedactors[key] as <T>(redact: RedactIdFn, val: T) => T;
+      const fn = dealPropertyRedactors[key] as <T>(redact: Redactor, val: T) => T;
       const val = fn(redact, v);
       log.writeLine(`    ${k}: ${val}`);
     }
   }
 
-  private _logActions(log: LogWriteStream, redact: RedactIdFn, actions: Action[]) {
+  private _logActions(log: LogWriteStream, redact: Redactor, actions: Action[]) {
     log.writeLine('Actions');
     for (const action of actions) {
       switch (action.type) {
@@ -80,23 +62,22 @@ export class DealDataLogger {
           this.printDealProperties(log, redact, action.properties);
           break;
         case 'update':
-          log.writeLine(`  Update: ${redact('D_', action.deal.id)}`);
+          log.writeLine(`  Update: ${redact.dealId(action.deal.id)}`);
           this.printDealProperties(log, redact, action.properties);
           break;
         case 'noop':
-          log.writeLine(`  Nothing: ${redact('D_', action.deal.id)}`);
+          log.writeLine(`  Nothing: ${redact.dealId(action.deal.id)}`);
           break;
       }
     }
   }
 
   logRecords(records: (License | Transaction)[]) {
-    const redact = this.redact.bind(this);
-    this._logRecords(this.plainLog, sameId, records);
-    this._logRecords(this.rededLog, redact, records);
+    this._logRecords(this.plainLog, this.nonRedactor, records);
+    this._logRecords(this.rededLog, this.shhRedactor, records);
   }
 
-  private _logRecords(log: LogWriteStream, redact: RedactIdFn, records: (License | Transaction)[]) {
+  private _logRecords(log: LogWriteStream, redact: Redactor, records: (License | Transaction)[]) {
     const ifTx = (fn: (r: Transaction) => string) =>
       (r: License | Transaction) =>
         r instanceof Transaction ? fn(r) : '';
@@ -108,27 +89,26 @@ export class DealDataLogger {
       rows: records,
       cols: [
         [{ title: 'Hosting' }, record => record.data.hosting],
-        [{ title: 'AddonLicenseId' }, record => redact('L_', record.data.addonLicenseId)],
+        [{ title: 'AddonLicenseId' }, record => redact.addonLicenseId(record.data.addonLicenseId)],
         [{ title: 'Date' }, record => record.data.maintenanceStartDate],
         [{ title: 'LicenseType' }, record => record.data.licenseType],
         [{ title: 'SaleType' }, ifTx(record => record.data.saleType)],
-        [{ title: 'Transaction' }, ifTx(record => redact('TX_', record.data.transactionId))],
+        [{ title: 'Transaction' }, ifTx(record => redact.transactionId(record.data.transactionId))],
         [{ title: 'Amount', align: 'right' }, ifTx(record => formatMoney(record.data.vendorAmount))],
       ],
     });
   }
 
   logEvents(events: DealRelevantEvent[]) {
-    const redact = this.redact.bind(this);
-    this._logEvents(this.plainLog, sameId, events);
-    this._logEvents(this.rededLog, redact, events);
+    this._logEvents(this.plainLog, this.nonRedactor, events);
+    this._logEvents(this.rededLog, this.shhRedactor, events);
   }
 
-  private _logEvents(log: LogWriteStream, redact: RedactIdFn, events: DealRelevantEvent[]) {
+  private _logEvents(log: LogWriteStream, redact: Redactor, events: DealRelevantEvent[]) {
     const rows = events.map(abbrEventDetails).map(({ type, lics, txs }) => ({
       type,
-      lics: lics.map(l => redact('L_', l)),
-      txs: txs.map(tx => redact('TX_', tx)),
+      lics: lics.map(l => redact.addonLicenseId(l)),
+      txs: txs.map(tx => redact.transactionId(tx)),
     }));
 
     Table.print({
@@ -141,6 +121,77 @@ export class DealDataLogger {
         [{ title: 'Transactions' }, row => row.txs?.join(', ') ?? ''],
       ],
     });
+  }
+
+}
+
+type R = string | undefined | null;
+
+interface Redactor {
+
+  addonLicenseId<T extends R>(val: T): T;
+  transactionId<T extends R>(val: T): T;
+  dealId<T extends R>(val: T): T;
+  appName<T extends R>(val: T): T;
+  dealName<T extends R>(val: T): T;
+  product<T extends R>(val: T): T;
+
+}
+
+class NonRedactor implements Redactor {
+
+  public addonLicenseId<T extends R>(val: T): T { return val; }
+  public transactionId<T extends R>(val: T): T { return val; }
+  public dealId<T extends R>(val: T): T { return val; }
+  public appName<T extends R>(val: T): T { return val; }
+  public dealName<T extends R>(val: T): T { return val; }
+  public product<T extends R>(val: T): T { return val; }
+
+}
+
+class PrivacyRedactor implements Redactor {
+
+  private redactions = new Map<string, string>();
+  private newIds = new Set<string>();
+
+  private redact<T extends R>(id: T, idgen: () => string): T {
+    if (id === undefined || id === null) return id;
+    let rid = this.redactions.get(id);
+    if (!rid) {
+      do { rid = idgen(); }
+      while (this.newIds.has(rid));
+      this.newIds.add(rid);
+      this.redactions.set(id, rid)
+    };
+    return rid as T;
+  }
+
+  private shortUUID() {
+    return uuidv4().replace(/-/g, '').slice(0, 10);
+  }
+
+  public addonLicenseId<T extends R>(val: T): T {
+    return this.redact(val, () => `L[${this.shortUUID()}]`);
+  }
+
+  public transactionId<T extends R>(val: T): T {
+    return this.redact(val, () => `TX[${this.shortUUID()}]`);
+  }
+
+  public dealId<T extends R>(val: T): T {
+    return this.redact(val, () => `D[${this.shortUUID()}]`);
+  }
+
+  public appName<T extends R>(val: T): T {
+    return this.redact(val, () => `AppName[${this.shortUUID()}]`);
+  }
+
+  public dealName<T extends R>(val: T): T {
+    return this.redact(val, () => `DealName[${this.shortUUID()}]`);
+  }
+
+  public product<T extends R>(val: T): T {
+    return this.redact(val, () => `Product[${this.shortUUID()}]`);
   }
 
 }
