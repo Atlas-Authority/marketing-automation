86b70cdb7ad0b19dfbbe36a1f644efc68b96b243
Steven Degutis <sbdegutis@gmail.com>
2021-12-23 15:09:16 -0600

    Creating another test case just to make sure this works.

--- src/lib/engine/deal-generator/test/test-cases/basics.test.ts
+++ src/lib/engine/deal-generator/test/test-cases/basics.test.ts
@@ -1,4 +1,4 @@
-import { runDealGenerator, runDealGeneratorTwice, TestInput, testLicense, testTransaction } from "../utils";
+import { runDealGenerator, runDealGeneratorTwice, testLicense, testTransaction } from "../utils";
 
 
 it(`Creates deal from purchase`, () => {
@@ -94,5 +94,43 @@ it(`Creates deals for renewals and upgrades separately from purchases`, () => {
         amount: 274
       }
     ]
+  ]);
+});
+
+it(`Does nothing when upgrades and renewals already have deals`, () => {
+  const { events, actions } = runDealGeneratorTwice({
+    group: [['L2169473', []], ['2479625', ['AT-131949332[2479625]', 'AT-97165138[2479625]']]],
+    deals: [],
+    records: [
+      testLicense("L2169473", "2013-01-21", "EVALUATION", "inactive"),
+      testLicense("2479625", "2013-01-23", "COMMERCIAL", "active"),
+      testTransaction("2479625", "2020-04-07", "COMMERCIAL", "Upgrade", "AT-97165138", 411),
+      testTransaction("2479625", "2021-03-25", "COMMERCIAL", "Renewal", "AT-131949332", 274)
+    ],
+  });
+  expect(events).toEqual([
+    ['purchase', 'L2169473', '2479625'],
+    ['upgrade', 'AT-97165138[2479625]'],
+    ['renewal', 'AT-131949332[2479625]']
+  ]);
+  expect(actions).toEqual([
+    [
+      "Nothing",
+      "2479625",
+      "CLOSED_WON",
+      0,
+    ],
+    [
+      "Nothing",
+      "AT-97165138[2479625]",
+      "CLOSED_WON",
+      411,
+    ],
+    [
+      "Nothing",
+      "AT-131949332[2479625]",
+      "CLOSED_WON",
+      274,
+    ],
   ]);
 });
