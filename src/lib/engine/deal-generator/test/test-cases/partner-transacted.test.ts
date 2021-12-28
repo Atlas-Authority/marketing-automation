e5d43b26978d1616252b74bfa75f25b0c1bcff6c
Steven Degutis <sbdegutis@gmail.com>
2021-12-28 16:52:54 -0600

    Adding another test case for partner-transacted deals.

--- src/lib/engine/deal-generator/test/test-cases/partner-transacted.test.ts
+++ src/lib/engine/deal-generator/test/test-cases/partner-transacted.test.ts
@@ -18,3 +18,21 @@ it(`Sets partner domain on deal if record is partern-transacted`, () => {
   const deal = deals[0];
   expect(deal.data.associatedPartner).toEqual(partnerDomain);
 });
+
+it(`Sets partner domain on deal if record is partern-transacted even if newer one is not`, () => {
+  const license1 = testLicense("1111111", "2021-12-26", "EVAL", "inactive");
+  const license2 = testLicense("2222222", "2021-12-27", "COMMERCIAL", "active");
+  const partnerDomain = license1.data.technicalContact.email.split('@')[1];
+
+  const { db } = runDealGeneratorTwice({
+    group: [['1111111', []], ['2222222', []]],
+    records: [license1, license2],
+    partnerDomains: [partnerDomain],
+  });
+
+  const deals = db.dealManager.getArray();
+  expect(deals.length).toBe(1);
+
+  const deal = deals[0];
+  expect(deal.data.associatedPartner).toEqual(partnerDomain);
+});
