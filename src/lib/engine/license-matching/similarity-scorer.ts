6a6f41a7b3222119ffe5614e06f65f279793c635
Steven Degutis <sbdegutis@gmail.com>
2022-01-07 16:29:08 -0600

    Slightly faster similarity algorithm.

--- src/lib/engine/license-matching/similarity-scorer.ts
+++ src/lib/engine/license-matching/similarity-scorer.ts
@@ -9,6 +9,7 @@ export class SimilarityScorer {
    */
   public score(atLeast: number, a: string | undefined, b: string | undefined) {
     if (!a || !b || !this.NON_EMPTY_FIELD.test(a) || !this.NON_EMPTY_FIELD.test(b)) return 0;
+    if (a === b) return 1;
     const score = diceCoefficient(a, b);
     if (score < atLeast) return 0;
     return score;
