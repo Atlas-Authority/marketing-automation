Atlas-Authority/marketing-automation

commit 962b49cb6699e470d9fc955cc8f81baae65b4165

Slight code cleanup in scoring engine.

src/lib/engine/license-matching/license-grouper.ts
@@ -22,7 +22,7 @@ export function matchIntoLikelyGroups(db: Database): RelatedLicenseSet[] {
   const productGroupings: Iterable<{ addonKey: string; hosting: string; group: License[] }> = groupMappingByProduct(itemsByAddonLicenseId);
 
   const { maybeMatches, unaccounted: unaccountedArray } = fnOrCache('scorer.json', () => {
-    const scorer = new LicenseMatcher(db);
+    const scorer = new LicenseMatcher();
     const { maybeMatches, unaccounted } = scoreLicenseMatches(productGroupings, scorer);
     return {
       maybeMatches,

src/lib/engine/license-matching/license-matcher.ts
@@ -1,4 +1,3 @@
-import { Database } from "../../model/database";
 import { License } from "../../model/license";
 import { SimilarityScorer } from "./similarity-scorer";
 
@@ -8,7 +7,7 @@ export class LicenseMatcher {
 
   private similarityScorer = new SimilarityScorer();
 
-  public constructor(private db: Database) { }
+  public constructor() { }
 
   public score(license1: License, license2: License): null | { item1: string, item2: string, score: number, reasons: string[] } {
     const item1 = license1.data.addonLicenseId;
@@ -27,20 +26,16 @@ export class LicenseMatcher {
       };
     }
 
-    const techEmail1 = license1.data.technicalContact.email;
-    const techEmail2 = license2.data.technicalContact.email;
+    const techContact1 = license1.techContact;
+    const techContact2 = license2.techContact;
 
-    const billingEmail1 = license1.data.billingContact?.email;
-    const billingEmail2 = license2.data.billingContact?.email;
-
-    const contact1 = this.db.contactManager.getByEmail(techEmail1);
-    const contact2 = this.db.contactManager.getByEmail(techEmail2);
+    const billingContact1 = license1.billingContact;
+    const billingContact2 = license2.billingContact;
 
     // If same exact email, definitely a match
     if (
-      (contact1 === contact2) ||
-      (techEmail1 === techEmail2) ||
-      (billingEmail1 && billingEmail1 === billingEmail2)
+      (techContact1 === techContact2) ||
+      (billingContact1 && billingContact1 === billingContact2)
     ) {
       return {
         item1,
@@ -51,8 +46,8 @@ export class LicenseMatcher {
     }
 
     if (
-      techEmail1 === billingEmail2 ||
-      techEmail2 === billingEmail1
+      techContact1 === billingContact2 ||
+      techContact2 === billingContact1
     ) {
       return {
         item1,
@@ -66,10 +61,10 @@ export class LicenseMatcher {
 
     const reasons: string[] = [];
 
-    const [emailAddress1, domain1] = techEmail1.split('@');
-    const [emailAddress2, domain2] = techEmail2.split('@');
+    const [emailAddress1, domain1] = techContact1.data.email.split('@');
+    const [emailAddress2, domain2] = techContact2.data.email.split('@');
 
-    if (!this.db.providerDomains.has(domain1)) {
+    if (techContact1.isCustomer) {
       const domainScore = Math.round(30 * this.similarityScorer.score(0.80,
         domain1.toLowerCase(),
         domain2.toLowerCase(),

commit 1a0df67e7e99db6377904ab011d75609b0ccaac7

Fixing slight regression from 962b49cb6699e470d9fc955cc8f81baae65b4165 based on misreading the word 'provider'.

src/lib/engine/license-matching/license-grouper.ts
@@ -24,7 +24,7 @@ export function matchIntoLikelyGroups(db: Database): RelatedLicenseSet[] {
   const threshold = 130;
 
   const { maybeMatches, unaccounted: unaccountedArray } = fnOrCache('scorer.json', () => {
-    const scorer = new LicenseMatcher();
+    const scorer = new LicenseMatcher(db.providerDomains);
     const { maybeMatches, unaccounted } = scoreLicenseMatches(threshold, productGroupings, scorer);
     return {
       maybeMatches,

src/lib/engine/license-matching/license-matcher.ts
@@ -7,7 +7,7 @@ export class LicenseMatcher {
 
   private similarityScorer = new SimilarityScorer();
 
-  public constructor() { }
+  public constructor(private providerDomains: Set<string>) { }
 
   public score(threshold: number, license1: License, license2: License, reasons: string[]): null | { score: number } {
     const item1 = license1.data.addonLicenseId;
@@ -73,7 +73,7 @@ export class LicenseMatcher {
     const [emailAddress1, domain1] = techContact1.data.email.split('@');
     const [emailAddress2, domain2] = techContact2.data.email.split('@');
 
-    if (techContact1.isCustomer) {
+    if (!this.providerDomains.has(domain1)) {
       const domainScore = Math.round(30 * this.similarityScorer.score(0.80,
         domain1.toLowerCase(),
         domain2.toLowerCase(),