00f9fa09ff9f26fd0124ce448d60562291470aef
Steven Degutis <sbdegutis@gmail.com>
2022-01-24 19:47:17 -0600

    Log file should say whether we are creating/updating an entity.

--- src/lib/hubspot/log-results.ts
+++ src/lib/hubspot/log-results.ts
@@ -6,45 +6,58 @@ import { Entity } from '../hubspot/entity';
 import { EntityKind } from '../hubspot/interfaces';
 
 export function logHubspotResults(hubspot: Hubspot, logFile: DataFile<any>) {
-  const ids: Record<EntityKind, number> = { deal: 0, contact: 0, company: 0 };
-
-  const createIdIfNeeded = <T extends Entity<any, any>>(entity: T) => {
-    if (!entity.id) {
-      const kind = entity.adapter.kind;
-      entity.id = `fake-${kind}-${++ids[kind]}`;
-    }
-  };
-
-  hubspot.dealManager.getArray().forEach(createIdIfNeeded);
-  hubspot.contactManager.getArray().forEach(createIdIfNeeded);
-  hubspot.companyManager.getArray().forEach(createIdIfNeeded);
-
-  logFile.writeStream(stream => {
-    hubspot.dealManager.getArray().forEach((entity) => logEntity(stream, entity));
-    hubspot.contactManager.getArray().forEach((entity) => logEntity(stream, entity));
-    hubspot.companyManager.getArray().forEach((entity) => logEntity(stream, entity));
-  });
+  new HubspotResultLogger().logResults(hubspot, logFile);
 }
 
-function logEntity(stream: LogWriteStream, entity: Entity<any, any>) {
-  const fromKind = entity.adapter.kind;
+class HubspotResultLogger {
 
-  const stringify = (o: any) => util.inspect(o, { depth: null, breakLength: 100 });
+  private ids: Record<EntityKind, number> = { deal: 0, contact: 0, company: 0 };
+  private creating = new Set<Entity<any, any>>();
 
-  const properties = entity.getPropertyChanges();
-  if (Object.keys(properties).length > 0) {
-    stream.writeLine(`Updating [${fromKind}:${entity.id!}]: ${stringify(properties)}\n`);
+  public logResults(hubspot: Hubspot, logFile: DataFile<any>) {
+    logFile.writeStream(stream => {
+      hubspot.dealManager.getArray().forEach((entity) => this.logEntity(stream, entity));
+      hubspot.contactManager.getArray().forEach((entity) => this.logEntity(stream, entity));
+      hubspot.companyManager.getArray().forEach((entity) => this.logEntity(stream, entity));
+    });
   }
 
-  const associations = entity.getAssociationChanges().filter(assoc => {
-    const otherKind = assoc.other.adapter.kind;
-    const found = entity.adapter.associations.find(a => a[0] === otherKind);
-    return found?.[1].includes('up');
+  private logEntity(stream: LogWriteStream, entity: Entity<any, any>) {
+    const fromKind = entity.adapter.kind;
+
+    const properties = entity.getPropertyChanges();
+    if (Object.keys(properties).length > 0) {
+      const verb = this.creating.has(entity) ? 'Creating' : 'Updating';
+      stream.writeLine(`${verb} [${fromKind}:${this.idFor(entity)}]:\n${stringify(properties)}\n`);
+    }
+
+    const associations = entity.getAssociationChanges().filter(assoc => {
+      const otherKind = assoc.other.adapter.kind;
+      const found = entity.adapter.associations.find(a => a[0] === otherKind);
+      return found?.[1].includes('up');
+    });
+    if (associations.length > 0) {
+      stream.writeLine(`Associating [${fromKind}:${this.idFor(entity)}]:\n${stringify(associations.map(assoc => [
+        assoc.op,
+        `${assoc.other.adapter.kind}:${this.idFor(assoc.other)}`,
+      ]))}\n`);
+    }
+  }
+
+  private idFor<T extends Entity<any, any>>(entity: T) {
+    if (!entity.id) {
+      const kind = entity.adapter.kind;
+      this.creating.add(entity);
+      entity.id = `fake-${++this.ids[kind]}`;
+    }
+    return entity.id;
+  }
+
+}
+
+function stringify(o: any) {
+  return util.inspect(o, {
+    depth: null,
+    breakLength: 80,
   });
-  if (associations.length > 0) {
-    stream.writeLine(`Associating [${fromKind}:${entity.id!}]: ${stringify(associations.map(assoc => [
-      assoc.op,
-      `${assoc.other.adapter.kind}:${assoc.other.id!}`,
-    ]))}\n`);
-  }
 }
