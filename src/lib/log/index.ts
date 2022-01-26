193e6d2b7e56c32b64e6aa43e0b66470e704e341
Steven Degutis <sbdegutis@gmail.com>
2022-01-26 09:12:21 -0600

    Making log files optional in Logger so it can at least print to stdout/stderr.

--- src/lib/log/index.ts
+++ src/lib/log/index.ts
@@ -16,29 +16,31 @@ export class Logger {
   #checkMatchGroupsLog;
   #hubspotResultLog;
 
-  constructor(logDir: DataDir) {
-    this.#licenseScoringFile = logDir.file('license-scoring.csv');
-    this.#dealGeneratorFile = logDir.file('deal-generator.txt');
-    this.#allMatchGroupsLog = logDir.file('matched-groups-all.csv');
-    this.#checkMatchGroupsLog = logDir.file('matched-groups-to-check.csv');
-    this.#hubspotResultLog = logDir.file('hubspot-out.txt');
+  constructor(logDir?: DataDir) {
+    this.#licenseScoringFile = this.#fast ? undefined : logDir?.file('license-scoring.csv');
+    this.#dealGeneratorFile = logDir?.file('deal-generator.txt');
+    this.#allMatchGroupsLog = logDir?.file('matched-groups-all.csv');
+    this.#checkMatchGroupsLog = logDir?.file('matched-groups-to-check.csv');
+    this.#hubspotResultLog = logDir?.file('hubspot-out.txt');
   }
 
   public scoreLogger() {
-    if (this.#fast) return;
+    if (!this.#licenseScoringFile) return;
     return new LicenseMatchLogger(this.#licenseScoringFile.writeCsvStream());
   }
 
   public dealGeneratorLog() {
+    if (!this.#dealGeneratorFile) return;
     return new DealDataLogger(this.#dealGeneratorFile.writeStream());
   }
 
   public hubspotOutputLogger() {
+    if (!this.#hubspotResultLog) return;
     return new HubspotOutputLogger(this.#hubspotResultLog);
   }
 
-  public allMatchGroupsLog() { return this.#allMatchGroupsLog.writeCsvStream() };
-  public checkMatchGroupsLog() { return this.#checkMatchGroupsLog.writeCsvStream() };
+  public allMatchGroupsLog() { return this.#allMatchGroupsLog?.writeCsvStream() };
+  public checkMatchGroupsLog() { return this.#checkMatchGroupsLog?.writeCsvStream() };
 
 
   public printError(prefix: string, ...args: any[]) { this.#consoleLogger.error(prefix, ...args); }
