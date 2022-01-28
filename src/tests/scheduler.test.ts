f08e6a28b571152c252fb7f214aa568d473448ae
Steven Degutis <sbdegutis@gmail.com>
2022-01-28 08:24:50 -0600

    Cleaning up scheduler test suite.

--- src/tests/scheduler.test.ts
+++ src/tests/scheduler.test.ts
@@ -2,6 +2,24 @@ import * as luxon from 'luxon';
 import { DataSetScheduler, parseSchedule } from "../lib/data/scheduler";
 import { sorter } from '../lib/util/helpers';
 
+const d1 = { timestamp: luxon.DateTime.fromISO('2020-05-17T09') };
+const d2 = { timestamp: luxon.DateTime.fromISO('2020-05-18T09') };
+const d3 = { timestamp: luxon.DateTime.fromISO('2020-05-19T09') };
+const d4 = { timestamp: luxon.DateTime.fromISO('2020-05-20T09') };
+const d5 = { timestamp: luxon.DateTime.fromISO('2020-05-21T09') };
+
+const w1 = { timestamp: luxon.DateTime.fromISO('2020-04-29T10') };
+const w2 = { timestamp: luxon.DateTime.fromISO('2020-05-06T10') };
+const w3 = { timestamp: luxon.DateTime.fromISO('2020-05-13T10') };
+const w4 = { timestamp: luxon.DateTime.fromISO('2020-05-20T08') };
+const w5 = { timestamp: luxon.DateTime.fromISO('2020-05-27T10') };
+
+const m1 = { timestamp: luxon.DateTime.fromISO('2020-02-20T11') };
+const m2 = { timestamp: luxon.DateTime.fromISO('2020-03-20T11') };
+const m3 = { timestamp: luxon.DateTime.fromISO('2020-04-20T11') };
+const m4 = { timestamp: luxon.DateTime.fromISO('2020-05-20T07') };
+const m5 = { timestamp: luxon.DateTime.fromISO('2020-06-20T11') };
+
 describe(`Scheduler`, () => {
 
   it(`Can parse syntax`, () => {
@@ -25,18 +43,10 @@ describe(`Scheduler`, () => {
       month: 0,
     });
 
-    const from = luxon.DateTime.fromISO('2020-01-04T11');
-
-    const t1 = { timestamp: from.minus({ days: 3 }) };
-    const t2 = { timestamp: from.minus({ days: 2 }) };
-    const t3 = { timestamp: from.minus({ days: 1 }) };
-    const t4 = { timestamp: from };
-    const t5 = { timestamp: from.plus({ days: 1 }) };
-
-    expect(scheduler.check(from,
-      [t1, t2, t3, t4, t5]
+    expect(scheduler.check(d4.timestamp,
+      [d1, d2, d3, d4, d5]
     )).toEqual(
-      [t2, t3, t4]
+      [d2, d3, d4]
     );
   });
 
@@ -47,18 +57,10 @@ describe(`Scheduler`, () => {
       month: 0,
     });
 
-    const from = luxon.DateTime.fromISO('2020-01-04T11');
-
-    const t1 = { timestamp: from.minus({ weeks: 3 }) };
-    const t2 = { timestamp: from.minus({ weeks: 2 }) };
-    const t3 = { timestamp: from.minus({ weeks: 1 }) };
-    const t4 = { timestamp: from };
-    const t5 = { timestamp: from.plus({ weeks: 1 }) };
-
-    expect(scheduler.check(from,
-      [t1, t2, t3, t4, t5]
+    expect(scheduler.check(w4.timestamp,
+      [w1, w2, w3, w4, w5]
     )).toEqual(
-      [t2, t3, t4]
+      [w2, w3, w4]
     );
   });
 
@@ -69,18 +71,10 @@ describe(`Scheduler`, () => {
       month: 3,
     });
 
-    const from = luxon.DateTime.fromISO('2020-01-04T11');
-
-    const t1 = { timestamp: from.minus({ months: 3 }) };
-    const t2 = { timestamp: from.minus({ months: 2 }) };
-    const t3 = { timestamp: from.minus({ months: 1 }) };
-    const t4 = { timestamp: from };
-    const t5 = { timestamp: from.plus({ months: 1 }) };
-
-    expect(scheduler.check(from,
-      [t1, t2, t3, t4, t5]
+    expect(scheduler.check(m4.timestamp,
+      [m1, m2, m3, m4, m5]
     )).toEqual(
-      [t2, t3, t4]
+      [m2, m3, m4]
     );
   });
 
@@ -91,27 +85,7 @@ describe(`Scheduler`, () => {
       month: 3,
     });
 
-    const from = luxon.DateTime.fromISO('2020-05-20T11');
-
-    const d1 = { timestamp: luxon.DateTime.fromISO('2020-05-17T09') };
-    const d2 = { timestamp: luxon.DateTime.fromISO('2020-05-18T09') }; // good
-    const d3 = { timestamp: luxon.DateTime.fromISO('2020-05-19T09') }; // good
-    const d4 = { timestamp: luxon.DateTime.fromISO('2020-05-20T09') }; // good
-    const d5 = { timestamp: luxon.DateTime.fromISO('2020-05-21T09') };
-
-    const w1 = { timestamp: luxon.DateTime.fromISO('2020-04-29T10') };
-    const w2 = { timestamp: luxon.DateTime.fromISO('2020-05-06T10') }; // good
-    const w3 = { timestamp: luxon.DateTime.fromISO('2020-05-13T10') }; // good
-    const w4 = { timestamp: luxon.DateTime.fromISO('2020-05-20T08') }; // good
-    const w5 = { timestamp: luxon.DateTime.fromISO('2020-05-27T10') };
-
-    const m1 = { timestamp: luxon.DateTime.fromISO('2020-02-20T11') };
-    const m2 = { timestamp: luxon.DateTime.fromISO('2020-03-20T11') }; // good
-    const m3 = { timestamp: luxon.DateTime.fromISO('2020-04-20T11') }; // good
-    const m4 = { timestamp: luxon.DateTime.fromISO('2020-05-20T07') }; // good
-    const m5 = { timestamp: luxon.DateTime.fromISO('2020-06-20T11') };
-
-    expect(scheduler.check(from, [
+    expect(scheduler.check(d4.timestamp.plus({ hour: 2 }), [
       d1, d2, d3, d4, d5,
       w1, w2, w3, w4, w5,
       m1, m2, m3, m4, m5,
@@ -129,27 +103,7 @@ describe(`Scheduler`, () => {
       month: 0,
     });
 
-    const from = luxon.DateTime.fromISO('2020-05-20T11');
-
-    const d1 = { timestamp: luxon.DateTime.fromISO('2020-05-17T09') };
-    const d2 = { timestamp: luxon.DateTime.fromISO('2020-05-18T09') }; // good
-    const d3 = { timestamp: luxon.DateTime.fromISO('2020-05-19T09') }; // good
-    const d4 = { timestamp: luxon.DateTime.fromISO('2020-05-20T09') }; // good
-    const d5 = { timestamp: luxon.DateTime.fromISO('2020-05-21T09') };
-
-    const w1 = { timestamp: luxon.DateTime.fromISO('2020-04-29T10') };
-    const w2 = { timestamp: luxon.DateTime.fromISO('2020-05-06T10') }; // good
-    const w3 = { timestamp: luxon.DateTime.fromISO('2020-05-13T10') }; // good
-    const w4 = { timestamp: luxon.DateTime.fromISO('2020-05-20T10') }; // good
-    const w5 = { timestamp: luxon.DateTime.fromISO('2020-05-27T10') };
-
-    const m1 = { timestamp: luxon.DateTime.fromISO('2020-02-20T11') };
-    const m2 = { timestamp: luxon.DateTime.fromISO('2020-03-20T11') }; // good
-    const m3 = { timestamp: luxon.DateTime.fromISO('2020-04-20T11') }; // good
-    const m4 = { timestamp: luxon.DateTime.fromISO('2020-05-20T11') }; // good
-    const m5 = { timestamp: luxon.DateTime.fromISO('2020-06-20T11') };
-
-    expect(scheduler.check(from, [
+    expect(scheduler.check(m4.timestamp, [
       d1, d2, d3, d4, d5,
       w1, w2, w3, w4, w5,
       m1, m2, m3, m4, m5,
@@ -165,7 +119,7 @@ describe(`Scheduler`, () => {
       month: 0,
     });
 
-    const from = luxon.DateTime.fromISO('2020-01-04T11');
+    const from = d4.timestamp;
 
     const t1 = { timestamp: from.minus({ hours: 3 }) };
     const t2 = { timestamp: from.minus({ hours: 2 }) };
