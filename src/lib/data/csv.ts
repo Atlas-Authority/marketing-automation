b99bbfd2a31f8c33183ecfd72e49e66bd415ae6f
Steven Degutis <sbdegutis@gmail.com>
2022-02-02 09:02:12 -0600

    Slightly faster unflatten.

--- src/lib/data/csv.ts
+++ src/lib/data/csv.ts
@@ -82,24 +82,50 @@ export class CsvStream {
 
 }
 
+const unflatteners = new Map<string, Unflattener>();
+
 function unflatten(keys: string[], vals: any[]) {
-  const o = Object.create(null);
+  const unflattenerKey = keys.join(',');
+  let unflattener = unflatteners.get(unflattenerKey);
+  if (!unflattener) unflatteners.set(unflattenerKey, unflattener = new Unflattener(keys));
+  return unflattener.run(vals);
+}
 
-  for (let i = 0; i < keys.length; i++) {
-    const key = keys[i];
-    const val = vals[i];
-    if (val === undefined || val === null) continue;
+class Unflattener {
 
-    let sub = o;
-    const parts = key.split('.');
-    const last = parts.pop()!;
-    const lastKey = +last >= 0 ? +last : last;
-    const isArray = typeof lastKey === 'number';
-    for (const part of parts) {
-      sub = sub[part] ??= (isArray ? [] : Object.create(null));
+  #specs: {
+    parts: string[],
+    isArray: boolean,
+    lastKey: string | number,
+  }[] = [];
+
+  constructor(keys: string[]) {
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      const parts = key.split('.');
+      const last = parts.pop()!;
+      const lastKey = +last >= 0 ? +last : last;
+      const isArray = typeof lastKey === 'number';
+      this.#specs.push({ parts, isArray, lastKey });
     }
-    sub[lastKey] = val;
   }
 
-  return o;
+  run(vals: any[]) {
+    const o = Object.create(null);
+
+    for (let i = 0; i < this.#specs.length; i++) {
+      const val = vals[i];
+      if (val === undefined || val === null) continue;
+
+      const { isArray, parts, lastKey } = this.#specs[i];
+      let sub = o;
+      for (let i = 0; i < parts.length; i++) {
+        sub = sub[parts[i]] ??= (isArray ? [] : Object.create(null));
+      }
+      sub[lastKey] = val;
+    }
+
+    return o;
+  }
+
 }
