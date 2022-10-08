0651d5192aef25abef6c01bf36dd1a8f43745b8b
Steven Degutis <sbdegutis@gmail.com>
2022-10-08 10:13:16 -0500

    Add both IDs to HubSpot association error messages.

--- src/lib/hubspot/api.ts
+++ src/lib/hubspot/api.ts
@@ -42,7 +42,7 @@ export default class HubspotAPI {
           .flatMap(([, { results }]) => (
             results.map(item => {
               const prefix = `${entityAdapter.kind}_to_`;
-              assert.ok(item.type.startsWith(prefix), `"${item.type}" does not start with "${prefix}"`);
+              assert.ok(item.type.startsWith(prefix), `"${item.type}" does not start with "${prefix}" for "${id}" to "${item.id}"`);
               const otherKind = item.type.substr(prefix.length) as EntityKind;
               return `${otherKind}:${item.id}` as RelativeAssociation;
             })
